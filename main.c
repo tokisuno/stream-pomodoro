328fec84bac7bab49caba49b2873f0139778e472
tokisuno <lucaswmei@gmail.com>
2025-01-21 10:30:46 -0500

    basic functionality and scene changing works!

--- main.c
+++ main.c
@@ -36,15 +36,27 @@ static void conversion (int seconds, struct t_time *h);
 int main(void)
 {
     InitWindow(SCREEN_W, SCREEN_H, "pomodoro");
+    InitAudioDevice();
+
+    Sound session_begin = LoadSound("resources/menu.ogg");
+    SetSoundVolume(session_begin, 0.7f);
+
+    Sound session_end = LoadSound("resources/ping.ogg");
+    SetSoundVolume(session_end, 0.7f);
+
+    Sound bonk = LoadSound("resources/bonk.ogg");
+
     SetExitKey(KEY_NULL); // makes sure you don't accidentally hit esc and close the program
 
     int framesCounter = 0;
-    Font fontTtf = LoadFontEx("resources/PixAntiqua.ttf", 32, 0, 250);
+    Font fontTtf = LoadFontEx("resources/PixAntiqua.ttf", 48, 0, 250);
     GameScreen currentScreen = MAIN;
 
-    int countdown = STUDY_TIMER * 60;
+    int study_mins = STUDY_TIMER;
+    int break_mins = BREAK_TIMER;
+    int session_counter = 0;
     struct t_time time;
-
+    bool sound_played = false;
     SetTargetFPS(60);
 
     // Main loop
@@ -53,8 +65,14 @@ int main(void)
         switch (currentScreen) {
             case MAIN:
             {
+                if (sound_played == false) {
+                    PlaySound(bonk);
+                    sound_played = true;
+                }
+
                 if (IsKeyPressed(KEY_ENTER) || IsGestureDetected(GESTURE_TAP)) {
                     currentScreen = STUDY;
+                    PlaySound(session_begin);
                 }
             } break;
             case STUDY:
@@ -62,19 +80,33 @@ int main(void)
                 framesCounter++;
                 if (framesCounter == 60) {
                     framesCounter = 0;
-                    countdown--;
-                    conversion(countdown, &time);
+                    study_mins--;
+                    conversion(study_mins, &time);
                 }
-                if (IsKeyPressed(KEY_ENTER) || IsGestureDetected(GESTURE_TAP)) {
+                if (study_mins == 0 || IsMouseButtonPressed(MOUSE_BUTTON_LEFT)) {
                     currentScreen = BREAK;
-                    countdown = STUDY_TIMER * 60;
-                    conversion(countdown, &time);
+                    study_mins = STUDY_TIMER;
+                    conversion(study_mins, &time);
+                    PlaySound(session_end);
                 }
             } break;
             case BREAK:
             {
-                if (IsKeyPressed(KEY_ENTER) || IsGestureDetected(GESTURE_TAP)) {
+                framesCounter++;
+                if (framesCounter == 60) {
+                    framesCounter = 0;
+                    break_mins--;
+                    conversion(break_mins, &time);
+                }
+                if (IsMouseButtonPressed(MOUSE_BUTTON_LEFT)) {
                     currentScreen = MAIN;
+                    sound_played = false;
+                    break_mins = BREAK_TIMER;
+                    conversion(break_mins, &time);
+                } else if (IsMouseButtonPressed(MOUSE_BUTTON_RIGHT)) {
+                    PlaySound(session_begin);
+                    currentScreen = STUDY;
+                    break_mins = BREAK_TIMER;
                 }
             } break;
             default: break;
@@ -86,19 +118,20 @@ int main(void)
             switch (currentScreen) {
                 case MAIN:
                 {
-                    DrawRectangle(0, 0, SCREEN_W, SCREEN_H, WHITE);
-                    DrawTextEx(fontTtf, "Ready?", (Vector2){ 20.0f, 100.0f }, (float)fontTtf.baseSize, 2, MAROON);
+                    DrawRectangle(0, 0, SCREEN_W, SCREEN_H, BLACK);
+                    DrawTextEx(fontTtf, "New session!", (Vector2){ 20.0f, 40.0f }, (float)fontTtf.baseSize + 3.0f, 2, YELLOW);
                 } break;
                 case STUDY:
                 {
-                    DrawRectangle(0, 0, SCREEN_W, SCREEN_H, WHITE);
-                    DrawTextEx(fontTtf, "Study time!", (Vector2){ 20.0f, 40.0f }, (float)fontTtf.baseSize, 2, MAROON);
-                    DrawTextEx(fontTtf, TextFormat("%.2d:%.2d", time.mins, time.secs), (Vector2){ 20.0f, 100.0f }, (float)fontTtf.baseSize, 2, MAROON);
+                    DrawRectangle(0, 0, SCREEN_W, SCREEN_H, BLACK);
+                    DrawTextEx(fontTtf, "Study time!", (Vector2){ 20.0f, 40.0f }, (float)fontTtf.baseSize + 3.0f, 2, YELLOW);
+                    DrawTextEx(fontTtf, TextFormat("%.2d:%.2d", time.mins, time.secs), (Vector2){ 20.0f, 100.0f }, (float)fontTtf.baseSize, 2, GOLD);
                 } break;
                 case BREAK:
                 {
                     DrawRectangle(0, 0, SCREEN_W, SCREEN_H, BLACK);
-                    DrawTextEx(fontTtf, "Break time!", (Vector2){ 20.0f, 100.0f }, (float)fontTtf.baseSize, 2, WHITE);
+                    DrawTextEx(fontTtf, "Break time!", (Vector2){ 20.0f, 40.0f }, (float)fontTtf.baseSize + 3.0f, 2, YELLOW);
+                    DrawTextEx(fontTtf, TextFormat("%.2d:%.2d", time.mins, time.secs), (Vector2){ 20.0f, 100.0f }, (float)fontTtf.baseSize, 2, GOLD);
                 } break;
                 default: break;
             }
@@ -106,6 +139,10 @@ int main(void)
         EndDrawing();
     }
     UnloadFont(fontTtf);
+    UnloadSound(session_begin);
+    UnloadSound(session_end);
+
+    CloseAudioDevice();
     CloseWindow();
 
     return 0;
