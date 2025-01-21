344785292659bce8fb068f09bf5cb61d60b56f68
tokisuno <lucaswmei@gmail.com>
2025-01-21 10:30:28 -0500

    calculate seconds in config

--- config.h
+++ config.h
@@ -2,7 +2,6 @@
 static const int SCREEN_W = 640;
 static const int SCREEN_H = 480;
 
-// Timers are in minutes
-static const int STUDY_TIMER = 50;
-static const int BREAK_TIMER = 10;
-static const int SESSIONS    = 5;
+// Times are [ minutes * seconds ]
+static const int STUDY_TIMER = 50 * 60;
+static const int BREAK_TIMER = 10 * 60;
